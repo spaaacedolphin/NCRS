34e3976fe8cd59d98b1d5677c9730c935225ffd5
spacedolphin <dolphin.lsm04@gmail.com>
2025-02-09 17:23:46 +0900

    golden section search

---
+++ gss.c
@@ -0,0 +1,51 @@
+#include <math.h>
+
+static const double r = (sqrt(5)-1)/2;
+
+double golden_section_search_max(double (*f)(double), double a, double b, double Delta_X){
+	double t = (log(Delta_X/(b-a))/log(r)) + 1;
+	int n = ceil(t);
+	
+	int i;
+	double x1 = a + r*(b-a);
+	double x2 = b - r*(b-a);
+	
+	for(i=0;i<n;i++){
+		if(f(x1)>f(x2)){
+			a = x2;
+			x2 = x1;
+			x1 = a + r*(b-a);
+		}
+		else{
+			b = x1;
+			x1 = x2;
+			x2 = b - r*(b-a);
+		}
+	}
+	
+	return (a+b)/2;
+}
+
+double golden_section_search_min(double (*f)(double), double a, double b, double Delta_X){
+	double t = (log(Delta_X/(b-a))/log(r)) + 1;
+	int n = ceil(t);
+	
+	int i;
+	double x1 = a + r*(b-a);
+	double x2 = b - r*(b-a);
+	
+	for(i=0;i<n;i++){
+		if(f(x1)<f(x2)){
+			a = x2;
+			x2 = x1;
+			x1 = a + r*(b-a);
+		}
+		else{
+			b = x1;
+			x1 = x2;
+			x2 = b - r*(b-a);
+		}
+	}
+	
+	return (a+b)/2;
+}
